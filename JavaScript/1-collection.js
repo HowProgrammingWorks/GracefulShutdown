d6ab5ec095d42cdccc293a29a6f4e3f9d68c21c2
tshemsedinov <timur.shemsedinov@gmail.com>
2019-09-12 00:09:00 +0300

    Send message to clients

--- JavaScript/1-collection.js
+++ JavaScript/1-collection.js
@@ -1,47 +1,68 @@
 'use strict';
 
 const http = require('http');
-const connections = new Set();
+const connections = new Map();
+
+const SERVER_PORT = 8000;
+const LONG_RESPONSE = 60000;
 
 const server = http.createServer((req, res) => {
+  console.log('New request');
+  connections.set(res.connection, res);
   setTimeout(() => {
     res.end('Example output');
-  }, 60000);
+  }, LONG_RESPONSE);
 });
 
 server.on('connection', connection => {
-  connections.add(connection);
+  console.log('New connection');
   connection.on('close', () => {
+    console.log('Close');
     connections.delete(connection);
   });
 });
 
-server.listen(8000);
+server.listen(SERVER_PORT);
 
-const showConnection = () => {
+const showConnections = () => {
   console.log('Connection:', [...connections.values()].length);
-  for (const connection of connections.values()) {
+  for (const connection of connections.keys()) {
     const { remoteAddress, remotePort } = connection;
     console.log(`  ${remoteAddress}:${remotePort}`);
   }
 };
 
-setInterval(showConnection, 1000);
-
-const gracefulShutdown = callback => {
-  server.close(callback);
-  for (const connection of connections.values()) {
+const closeConnections = () => {
+  for (const [connection, res] of connections.entries()) {
     connections.delete(connection);
+    res.end('Server stopped');
     connection.destroy();
   }
 };
 
+const freeResources = callback => {
+  console.log('Free resources');
+  callback();
+};
+
+const gracefulShutdown = callback => {
+  server.close(error => {
+    if (error) {
+      console.log(error);
+      process.exit(1);
+    }
+    freeResources(callback);
+  });
+  closeConnections();
+};
+
 process.on('SIGINT', () => {
   console.log();
-  console.log('graceful shutdown');
+  console.log('Graceful shutdown');
+  showConnections();
   gracefulShutdown(() => {
-    showConnection();
-    console.log('bye');
+    showConnections();
+    console.log('Bye');
     process.exit(0);
   });
 });
